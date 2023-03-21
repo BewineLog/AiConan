51bc4839035301f0e3d38b4d938b23bd687b2be3
psm9718 <psm9718@gmail.com>
2023-03-21 12:08:50 +0900

    chore: 기본적인 springboot 환경 세팅

---
+++ be/src/test/java/com/capstone/aiconan/AiConanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.aiconan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiConanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
