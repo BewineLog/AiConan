894c9858eb90208fb95d033374b342ef6b819d7a
psm9718@gmail.com <psm9718@gmail.com>
2023-04-02 21:29:43 +0900

    feat: DetectionResponse 응답 객체 작업

--- be/src/main/java/com/capstone/aiconan/response/DetectionResponse.java
+++ be/src/main/java/com/capstone/aiconan/response/DetectionResponse.java
@@ -1,5 +1,26 @@
 package com.capstone.aiconan.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * packet sample
+ * Timestamp: 1479121434.854108
+ * ID: 0545    000
+ * DLC: 8
+ * d8 00 00 8a 00 00 00 00
+ */
+@Getter
 public class DetectionResponse {
 
+    private final String CANNetId;
+    private final String DLC;
+    private final String data;
+
+    @Builder
+    public DetectionResponse(String CANNetId, String DLC, String data) {
+        this.CANNetId = CANNetId;
+        this.DLC = DLC;
+        this.data = data;
+    }
 }
