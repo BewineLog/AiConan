e040b24dddd84fa3537b2816afaf56eff89673e6
psm9718@gmail.com <psm9718@gmail.com>
2023-04-09 22:41:09 +0900

    fix: detectionservicetest

--- be/src/test/java/com/capstone/aiconan/service/DetectionServiceTest.java
+++ be/src/test/java/com/capstone/aiconan/service/DetectionServiceTest.java
@@ -3,7 +3,6 @@ package com.capstone.aiconan.service;
 import com.capstone.aiconan.repository.DetectionRepository;
 import com.capstone.aiconan.repository.TypesRepository;
 import com.capstone.aiconan.response.DetectionResponse;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.transaction.Transactional;
 
 import static com.capstone.aiconan.domain.TypeOfAttack.DOS;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -29,15 +29,15 @@ class DetectionServiceTest {
     @Test
     @DisplayName("공격 패킷 저장 테스트")
     void recordAbnormalPackets() throws Exception {
-
         DetectionResponse flaskResponse = DetectionResponse.builder()
                 .CANNetId("123")
                 .DLC("123")
                 .typeOfAttack(DOS)
                 .build();
+
         detectionService.recordAbnormalPackets(flaskResponse);
 
-        Assertions.assertThat(detectionRepository.count()).isEqualTo(1);
+        assertThat(detectionRepository.count()).isEqualTo(1);
     }
 
 
