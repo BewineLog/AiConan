9be9ad919b861d576fa2847e8af5ff8f6918cff8
programsec-bud <ljh98@ajou.ac.kr>
2023-05-07 23:46:01 +0900

    fix: model detection error

--- server/main.py
+++ server/main.py
@@ -98,7 +98,7 @@ def detect():
     # for row in csv_data.values:
     result = model_detection(np_data)  # binary classification using AI 0: normal 1:  attack
     # unique, counts = np.unique(tf.round(result).numpy(), return_counts=True)
-    noa = Counter(result.round().tolist())[1.0]
+    noa = Counter(result.round().numpy().tolist())[1.0]
             # response = request.post('http://your-url.com/endpoint', data=row.to_json())
     resp['numberOfAttack'] = noa
     app.logger.info('binary classification success')
