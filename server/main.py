f7aafe082f6152b0194a5d140b4123441bbfcae8
programsec-bud <ljh98@ajou.ac.kr>
2023-05-07 20:11:00 +0900

    feat: binary classification model

--- server/main.py
+++ server/main.py
@@ -16,19 +16,22 @@ import torch
 import pickle
 from model import Model
 
+#add
+from collections import Counter
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # 이진 분류 모델 파일 불러오기
-with open('/home/ec2-user/environment/AiConan/model/scaler.pkl', 'rb') as f:
+with open('/home/ec2-user/environment/AiConan/model/binary_scaler.pkl', 'rb') as f:
     scaler = pickle.load(f)  # Timestamp scaler
-with open('/home/ec2-user/environment/AiConan/model/time_scaler.pkl', 'rb') as f:
+with open('/home/ec2-user/environment/AiConan/model/binary_time_scaler.pkl', 'rb') as f:
     time_scaler = pickle.load(f)  # Timestamp scaler
 
 # model_bl = load_model('/home/ec2-user/environment/AiConan/model/Timeseries_binary_classification(LSTM)98.02.h5')  # learning for binary classification
 # model_bc = load_model('/home/ec2-user/environment/AiConan/model/Timeseries_binary_classification(CLF)98.02.h5')  # binary classification
-model = load_model('/home/ec2-user/environment/AiConan/model/model.h5')
+model = load_model('/home/ec2-user/environment/AiConan/model/binary_model.h5')
 
 # # 다중 분류 모델 파일 불러오기
 # model_mc = torch.load('/home/ec2-user/environment/AiConan/model/model.pth', map_location=torch.device('cpu'))
@@ -61,18 +64,14 @@ def detect():
     noa = 0  # # of attack
     data = request.files['file']  # get csv file from web, file name in ['']
 
-
-    import io
-    # csv_data = io.StringIO(data.stream.read().decode("UTF8"))
     csv_data = pd.read_csv(data)
+    df_row = pd.DataFrame(csv_data, columns=csv_data.columns)
+    np_data = data_transform_for_detection(df_row)
     resp = dict()
-    for row in csv_data.values:
-    # row: 해당 행의 데이터 (1차원 배열 형식)
-        df_row = pd.DataFrame([row], columns=csv_data.columns)
-        np_data = data_transform_for_detection(df_row)
-        result = model_detection(np_data)  # binary classification using AI 0: normal 1:  attack
-        if int(np.argmax(result)) >= 1:
-            noa += 1
+
+    # for row in csv_data.values:
+    result = model_detection(np_data)  # binary classification using AI 0: normal 1:  attack
+    noa = Counter(result.round().tolist())[1.0]
             # response = request.post('http://your-url.com/endpoint', data=row.to_json())
     resp['numberOfAttack'] = noa
     app.logger.info('binary classification success')
