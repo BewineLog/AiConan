d5e930b778d4ca6db066254bc57058cbb594d6a3
psm9718 <psm9718@gmail.com>
2023-05-17 11:54:36 +0000

    refactor: login, stat features

--- server/webapp.py
+++ server/webapp.py
@@ -5,151 +5,60 @@ import streamlit as st
 import requests
 import altair as alt
 import json
-# from flask import redirect, url_for
 from streamlit_echarts import st_echarts
-
-###################################
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
 
-###################################
-
-# 모델 API endpoint
 api = "127.0.0.1"
 url = f'http://{api}:8000'
-predict_endpoint = '/'
-shap_endpoint = '/model/calculate-shap-values/'
 
+class LoginPage:
+    def __init__(self):
+        self.userId = None
+        self.password = None
 
-def _max_width_():
-    max_width_str = f"max-width: 1800px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True,
-    )
-    
-# @st.experimental_memo(ttl=60 * 60 * 24)
-# def create_chart(data):
-#     # Group data by attack type
-#     groups = data.groupby("attack_type_id")
-
-#     charts = []
-
-#     # Create a separate chart for each group
-#     for name, group in groups:
-#         chart = (
-#             alt.Chart(group, height=500, title=f"Attack Type {name}")
-#             .mark_line()
-#             .encode(
-#                 x=alt.X("timestamp:T", title="Date"),
-#                 y=alt.Y("count():Q", title="Packets"),
-#             )
-#             .interactive()
-#         )
-
-#         charts.append(chart)
-
-#     # Combine all charts into a single chart
-#     chart = alt.vconcat(*charts)
-
-    # return chart
-
-# define login page interface
-def login():
-    st.title('Admin Login')
-    userId = st.text_input('userId')
-    password = st.text_input('password', type="password")
-    
-    if st.button('Login'):
-        response = requests.post(url + "/authenticate", data={'userId': userId, 'password': password})
+    def render(self):
+        st.title('Admin Login')
+        self.userId = st.text_input('userId', key="login-userid")
+        self.password = st.text_input('password', type="password", key='login-password')
         
-        if response.status_code == 200 and 'token' in response.json():
-            st.success('Login successful')
-            st.session_state.token = response.json()['token']
-            admin_page()
-        else:
-            st.error('Invalid user ID or password.')
+        if st.button('Login'):
+            response = requests.post(url + "/authenticate", data={'userId': self.userId, 'password': self.password})
             
+            if response.status_code == 200 and 'token' in response.json():
+                st.success('Login successful')
+                st.session_state.token = response.json()['token']
+                return True
+            else:
+                st.error('Invalid user ID or password.')
+        return False
+
+def fetch_data():
+    response = requests.get(url + '/api/data')
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        data = pd.DataFrame(data)
+        return data
+    return None
 
 def create_chart(data):
     chart = alt.Chart(data).mark_bar().encode(
         x=alt.X("attack_types_id:N", axis=alt.Axis(title="Attack Type")),
         y=alt.Y("count():Q", axis=alt.Axis(title="Count")),
     ).properties(width=700, height=400)
-    
     return chart
 
-import altair as alt
-
-
-def create_graph(data):
-    # Group data by attack type
-    attack_type_names = {
-        1: "Normal",
-        2: "DOS",
-        3: "FUZZING",
-        4: "SPOOFING"
-    }
-    
-    groups = data.groupby("attack_types_id")
-
-    charts = []
-
-    # Create a separate chart for each group
-    for name, group in groups:
-        print(name, " : ", group)
-        chart = (
-            alt.Chart(group, height=500, title=f"Attack Type {attack_type_names[name]}")
-            .mark_line()
-            .encode(
-                x=alt.X("timestamp:T", title="timestamp"),
-                y=alt.Y("count():Q", title="Packets"),
-            )
-            .interactive()
-        )
-
-        charts.append(chart)
-
-    # Combine all charts into a single chart
-    chart = alt.vconcat(*charts)
-
-    return chart
-
-def admin_page():
-    print(">> welcome to admin page")
-    st.title("AIConan service Admin Page")
-
-    if st.button('Logout'):
-        headers = {'Authorization': 'Bearer ' + st.session_state.token}
-        response = requests.get(url + '/logout', headers=headers)
-        if response.status_code == 200:
-            st.success('Logout successful')
-            st.session_state.token = None
-            login()
-        else:
-            st.error('Logout failed')
-    
-    
-    response = requests.get(url + '/api/data')
-    print(">> monitoring graph")
-    if response.status_code == 200:
-        print(response.text)
-
-        # Parse JSON data
-        data = json.loads(response.text)
-
-        # Convert to DataFrame
-        data = pd.DataFrame(data)
-
+def admin_content():
+    # Fetch data button and other components...
+    if st.button('Fetch Data'):
+        with st.spinner('Loading data...'):
+            data = fetch_data()
+            if data is not None:
+                st.success('Data fetched successfully!')
+                st.dataframe(data)
+            else:
+                st.error('Error fetching data.')
+                
         # Display table
-        # data = pd.read_csv(io.StringIO(response.text))
-        
         st.subheader("Packet Table")
         st.text("")
         gb = GridOptionsBuilder.from_dataframe(data)
@@ -166,125 +75,47 @@ def admin_page():
             fit_columns_on_grid_load=False,
         )
         
-    
-        # st.subheader("Packet Graph 👇")
-        # st.text("")
-        # chart = create_graph(data)
-        # st.altair_chart(chart, use_container_width=True)
-
-        # st.text("")
-        # df = pd.DataFrame(response["selected_rows"])
-        # st.table(df)
-        # st.text("")
-        
         st.subheader("Packet Chart 👇")
         st.text("")
         chart = create_chart(data)
         st.altair_chart(chart, use_container_width=True)
         
         st.success("Data processed successfully!")
-        
-    else:
-        st.error("Error fetching data from Flask API.")
-    
+
+def admin_page():
+    st.title("AIConan Service Admin Page")
+
+    if st.button('Logout'):
+        headers = {'Authorization': 'Bearer ' + st.session_state.token}
+        response = requests.get(url + '/logout', headers=headers)
+        if response.status_code == 200:
+            st.session_state.token = None
+            st.success('Logout successful. Please login again.')
+            st.experimental_rerun()
+        else:
+            st.error('Logout failed')
+
+    if 'token' in st.session_state and st.session_state.token is not None:
+        admin_content()
 
 def user_page():
-    
-    st.title("AI Conan Service")
-    
-    c29, c30, c31 = st.columns([1, 6, 1])
-    
-    with c30:
-        uploaded_file = st.file_uploader(
-            "",
-            key="1",
-            help="Upload .csv file",
-        )
-        
-        input_user_name = st.text_input(label="User Name", value="default")
-        user_input = {"username" : input_user_name}
-        
-        if uploaded_file is not None:
-            # Check inserted .csv file
-            file_container = st.expander("Check your uploaded .csv")
-            shows = pd.read_csv(uploaded_file)
-            uploaded_file.seek(0)
-            file_container.write(shows)
-        
-        if st.button("Start Detection"):
-            if uploaded_file is not None:
-                # Send POST request to Flask API with CSV file
-                files = {'file': uploaded_file.getvalue()}
-                response = requests.post(url + "/api/detection", files=files, data=user_input)
-                
-                # Check response status
-                if response.status_code == 200:
-                    # Check response content for "DoS Attack Detected" message
-                    
-                    response_json = json.loads(response.text)
-                    number_of_attack = response_json.split(' ')[1][:-1]
-                    print(">> ", number_of_attack)
-                    number_of_attack = int(number_of_attack)
-                    if number_of_attack > 0:
-                        st.warning(f"{number_of_attack} Attack Detected!")
-                    else:
-                        st.success("💡 Detection Finished!")
-                else:
-                    st.error("Error uploading CSV file.")
-            
-        
-        else:
-            st.info(
-                f"""
-                    👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
-                    """
-            )
-            
-            st.stop()
-         
-         
-            
+    st.title("User Page")
 
-       
-    
-        
-        ###################################
-        
-        # 통계자료 다운로드 가능하도록 구현
-        # from functionforDownloadButtons import download_button
-    
-        ###################################
-        # c29, c30, c31 = st.columns([1, 1, 2])
-        # with c29:
-        
-        #     CSVButton = download_button(
-        #         df,
-        #         "File.csv",
-        #         "Download to CSV",
-        #     )
-        
-        # with c30:
-        #     CSVButton = download_button(
-        #         df,
-        #         "File.csv",
-        #         "Download to TXT",
-        # )
-        
-
-# Define a function to show the selected page
 def show_page(page):
     if page == "User Page":
         user_page()
     elif page == "Admin Page":
-        if 'user_id' not in st.session_state:
-          login()
-    else:
+        if 'token' not in st.session_state or st.session_state.token is None:
+            if not LoginPage().render():
+                return
         admin_page()
 
 # Set the app page configuration
-st.set_page_config( page_title="AIConan Detecting Service", page_icon="favicon.ico")
-
+st.set_page_config(page_title="AIConan Detecting Service", page_icon="favicon.ico")
 
 # Create a sidebar to switch between pages
 selected_page = st.sidebar.selectbox("Select a page", ("User Page", "Admin Page"))
-show_page(selected_page)
+
+# Run the app
+if __name__ == "__main__":
+    show_page(selected_page)
